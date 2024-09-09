176fbc7327b00343adffad9fccdfdb78308f911d
Kheng-Uzamaki <khenguzamiki1@gmail.com>
2024-09-10 00:03:37 +0700

    add more skill

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,10 +1,41 @@
 import styles from "./ContactStyle.module.css";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_zm5uxak", "template_8pwzysv", form.current, {
+        publicKey: "V3xrx3aQ1fYZCv2Ys",
+      })
+      .then(
+        () => {
+          toast.success("Your message has been sent successfully!",{
+            autoClose:3000
+          }); // Success toast
+
+          form.current.reset(); // Clear form
+        },
+        (error) => {
+          toast.error("Failed to send the message, please try again later.",{
+            autoClose:3000
+          }); // Error toast
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="">
+      <form ref={form} onSubmit={sendEmail}>
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
@@ -40,8 +71,9 @@ function Contact() {
             required
           ></textarea>
         </div>
-        <input type="submit" className="hover btn" value='Submit' />
+        <input type="submit" className="hover btn" value="Submit" />
       </form>
+      <ToastContainer />
     </section>
   );
 }
--- src/sections/Skill/Skills.jsx
+++ src/sections/Skill/Skills.jsx
@@ -1,32 +1,51 @@
 import styles from "./SkillsStyles.module.css";
-import checkMarkIcon from '../../assets/checkmark-dark.svg'
+import { useTheme } from "../../Common/ThemeContext";
+import checkMarkDark from '../../assets/checkmark-dark.svg'
+import checkMarkLight from '../../assets/checkmark-light.svg'
 import SkillList from "../../Common/SkillList";
 
 const Skills = () => {
+  const { theme } = useTheme();
+  const checkMarkIcon = theme === "light" ? checkMarkLight : checkMarkDark;
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="HTML" />
         <SkillList src={checkMarkIcon} skill="CSS" />
-        <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="TypeScript" />
+        <SkillList src={checkMarkIcon} skill="C++" />
+        <SkillList src={checkMarkIcon} skill="Bootstrap" />
         <SkillList src={checkMarkIcon} skill="NodeJs" />
       </div>
       <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="React" />
-        <SkillList src={checkMarkIcon} skill="Angular" />
-        <SkillList src={checkMarkIcon} skill="Vue" />
-        <SkillList src={checkMarkIcon} skill="Taiwind CSS" />
+        <SkillList src={checkMarkIcon} skill="ExpressJs" />
+        <SkillList src={checkMarkIcon} skill="PHP" />
+        <SkillList src={checkMarkIcon} skill="MySQL" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="Redux" />
-        <SkillList src={checkMarkIcon} skill="Webpack" />
         <SkillList src={checkMarkIcon} skill="Git" />
-        <SkillList src={checkMarkIcon} skill="Jest" />
-        <SkillList src={checkMarkIcon} skill="Bootsrap" />
+        <SkillList src={checkMarkIcon} skill="C#" />
+        <SkillList src={checkMarkIcon} skill="Laravel" />
+        <SkillList src={checkMarkIcon} skill="Taiwind" />
+        <SkillList src={checkMarkIcon} skill="JavaScript" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Java" />
+        <SkillList src={checkMarkIcon} skill="AWS/EC2" />
+        <SkillList src={checkMarkIcon} skill="Ai/ChatGPT" />
+        <SkillList src={checkMarkIcon} skill="Postgres" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Springboot" />
+        <SkillList src={checkMarkIcon} skill="WindowOs" />
+        <SkillList src={checkMarkIcon} skill="CentOs/Ubuntu" />
+        <SkillList src={checkMarkIcon} skill="Window Server" />
+        <SkillList src={checkMarkIcon} skill="Microsoft Office" />
       </div>
     </section>
   );
