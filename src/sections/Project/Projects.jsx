1421d31f827a6149f268dd664110edb86d45bef3
Kheng-Uzamaki <khenguzamiki1@gmail.com>
2024-09-09 16:09:07 +0700

    complete project section

--- src/sections/Project/Projects.jsx
+++ src/sections/Project/Projects.jsx
@@ -1,16 +1,19 @@
 import styles from "./ProjectsStyle.module.css";
 import viberr from "../../assets/viberr.png";
+import ProjectCard from "../../Common/ProjectCard";
+import freshBurger from "../../assets/fresh-burger.png"
+import hipsster from '../../assets/hipsster.png'
+import fitlift from '../../assets/fitlift.png'
 
 const Projects = () => {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <a href="https://github.com" target="_blank">
-          <img className="hover" src={viberr} alt="project" />
-          <h3>Viberr</h3>
-          <p>Streaming App</p>
-        </a>
+        <ProjectCard src={viberr} link='https:/github.com'  h3='Viberr' p='Streaming App'/>
+        <ProjectCard src={freshBurger} link='https:/github.com'  h3='Fresh Burger' p='Burger Restaurant'/>
+        <ProjectCard src={hipsster} link='https:/github.com'  h3='Hipsster' p='Glasses Shop'/>
+        <ProjectCard src={fitlift} link='https:/github.com'  h3='FitLift' p='Fitness App'/>
       </div>
     </section>
   );
